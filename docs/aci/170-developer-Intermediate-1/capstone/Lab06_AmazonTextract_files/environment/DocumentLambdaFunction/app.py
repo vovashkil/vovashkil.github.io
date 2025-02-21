04c55c58b8e3794fa5d0e1eacdb1e9f664619c94
Vova Shkil <vshkil@gmail.com>
2025-02-21 13:19:50 -0800

    DI1 Capstone Lab06 is complete

---
+++ docs/aci/170-developer-Intermediate-1/capstone/Lab06_AmazonTextract_files/environment/DocumentLambdaFunction/app.py
@@ -0,0 +1,174 @@
+"Lambda function to process .zip file"
+# Environment variables:
+# TABLE = CustomerMetaDataTable
+# TOPIC = SNS arn
+# QUEUE_URL = SQS URL
+
+import json
+import os
+import csv
+import zipfile
+import boto3
+
+unzipped_dir = "/tmp/unzipped/"
+unzipped_s3_prefix = "unzipped/"
+env_table = os.environ['TABLE']
+env_topic = os.environ['TOPIC']
+
+s3 = boto3.client('s3')
+dynamodb = boto3.resource('dynamodb')
+ddb_table = dynamodb.Table(env_table)
+rekognition = boto3.client('rekognition')
+sns = boto3.client('sns')
+textract = boto3.client('textract')
+
+def unzip_object(bucket, key):
+    """Download .zip file, extract, return bucket name, object names, app_uuid,
+    delete .zip file, and uploading objects to incoming"""
+    zip_name = os.path.basename(key)
+    zip_fullpath = f'/tmp/{zip_name}'
+    s3.download_file(bucket, key, zip_fullpath)
+    with zipfile.ZipFile(zip_fullpath, 'r') as zip_ref:
+        zip_ref.extractall(unzipped_dir)
+    os.remove(zip_fullpath)
+
+    zipped_files = os.listdir(unzipped_dir)
+    return zipped_files
+
+def parse_csv_ddb(app_uuid, details_file):
+    "Load CSV and save to dynamo"
+    with open(details_file, 'r', encoding="utf-8") as file:
+        reader = csv.DictReader(file)
+        details_dict = next(reader)
+
+    ddb_table.put_item(Item={**details_dict, "APP_UUID": app_uuid})
+
+    return details_dict
+
+def compare_faces(app_uuid, bucket, license_key, selfie_key):
+    "calls rekognition to compare license and selfie"
+    print("Starting face comparison")
+    compare_response = rekognition.compare_faces(
+        SourceImage={'S3Object': {
+            'Bucket': bucket,
+            'Name': license_key,
+        }},
+        TargetImage={'S3Object': {
+            'Bucket': bucket,
+            'Name': selfie_key,
+        }},
+        SimilarityThreshold=80
+    )
+
+    if len(compare_response['FaceMatches']) < 1:
+        photo_match_result = False
+    else:
+        photo_match_result = compare_response['FaceMatches'][0]['Similarity'] >= 80
+
+    # Update DDB with photo match value.
+    ddb_table.update_item(
+        Key={
+            'APP_UUID': app_uuid
+            },
+        UpdateExpression='SET LICENSE_SELFIE_MATCH = :p_match',
+        ExpressionAttributeValues={
+            ':p_match': photo_match_result
+            }
+        )
+
+    # Amazon SNS publish and Amazon S3 folder.
+    if not photo_match_result:
+        sns.publish(
+            TopicArn= env_topic,
+            Message= 'License photo validation FAILED',
+            Subject='License photo validation FAILED',
+            )
+
+    print("finished compare faces")
+    return photo_match_result
+
+def textract_response(bucket, license_key):
+    "Send license to textract"
+
+    # Pass the license image to Amazon Textract to extract information.
+    print("Starting license analysis")
+    response = textract.analyze_id(
+        DocumentPages=[{
+                'S3Object': {
+                'Bucket': bucket,
+                'Name': license_key
+            }
+        }]
+    )
+
+    id_document = response['IdentityDocuments'][0]
+    id_data = id_document['IdentityDocumentFields']
+    id_fields = {}
+
+    # Build a dictionary of the .csv fields and Amazon Textract values.
+    csv_fields = ['DOCUMENT_NUMBER','FIRST_NAME','LAST_NAME','DATE_OF_BIRTH',
+                    'ADDRESS','STATE_IN_ADDRESS','CITY_IN_ADDRESS','ZIP_CODE_IN_ADDRESS']
+    id_fields = { field['Type']['Text']: field['ValueDetection']['Text'] for field in id_data
+                if field['Type']['Text'] in csv_fields }
+    print("finished textract")
+    return id_fields
+
+def compare_dictionaries(app_uuid, details_dict, textract_dict):
+    "Compare license and csv"
+    print("Comparing license and csv")
+
+    comparison = details_dict == textract_dict
+
+    # Update DDB with pdata match value, SNS publish, and S3 folder.
+    ddb_table.update_item(
+        Key={
+            'APP_UUID': app_uuid
+            },
+        UpdateExpression='SET LICENSE_DETAILS_MATCH = :d_match',
+        ExpressionAttributeValues={
+            ':d_match': comparison
+            }
+        )
+
+    if not comparison:
+        sns.publish(
+            TopicArn= env_topic,
+            Message= 'Data validation between the license and the .csv file FAILED',
+            Subject='Data validation between the license and the .csv file FAILED',
+            )
+        raise ValueError('Data comparison between App and license FAILED. Program will stop')
+
+    return comparison
+
+def lambda_handler(event, _):
+    "Main lambda handler"
+    record = event['Records'][0]
+    bucket = record['s3']['bucket']['name']
+    key = record['s3']['object']['key']
+
+    # Unzip the object from the event.
+    files_list = unzip_object(bucket, key)
+
+    # Upload files to the unzipped location.
+    for file in files_list:
+        s3.upload_file(unzipped_dir + file, bucket, unzipped_s3_prefix + file)
+
+    # Retrieve app_uuid from the .zip file.
+    app_uuid = os.path.basename(key).replace(".zip", "")
+    selfie_key = f"{unzipped_s3_prefix}{app_uuid}_selfie.png"
+    license_key = f"{unzipped_s3_prefix}{app_uuid}_license.png"
+
+    # Save the .csv file to DynamoDB.
+    details_file = f"{unzipped_dir}{app_uuid}_details.csv"
+    details_dict = parse_csv_ddb(app_uuid, details_file)
+
+    # Submit license and selfie to Amazon Rekognition to compare faces.
+    rekog_response = compare_faces(app_uuid, bucket, license_key, selfie_key)
+    if not rekog_response:
+        raise ValueError('Photo rekognition match FAILED. Program will stop')
+
+    # Submit license to Amazon Textract to extract fields.
+    textract_dict = textract_response(bucket, license_key)
+
+    # Compare the Amazon Textract fields with the .csv file.
+    compare_dictionaries(app_uuid, details_dict, textract_dict)
